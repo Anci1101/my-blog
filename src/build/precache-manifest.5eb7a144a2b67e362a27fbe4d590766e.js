cbb55f242c6d4032e5a99f8c02fcb015ea94a886
Ana <anci11011@gmail.com>
2020-09-15 17:08:15 +0200

    first commit

---
+++ src/build/precache-manifest.5eb7a144a2b67e362a27fbe4d590766e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5369b100ec52414fd602d65e31ef41c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dbf17bdf7f03986e400",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9f27527568c48733aa8c",
+    "url": "/static/js/2.ca0bc356.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca0bc356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbf17bdf7f03986e400",
+    "url": "/static/js/main.bbc6c6ed.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
